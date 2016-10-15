e59e8824728f9f016426a0562914ad40023c1861
Portia Sharma <portiasharma@macbook-pro.local>
2016-10-15 02:06:33 -0700

    Added share action

--- eyeFind/DetailViewController.swift
+++ eyeFind/DetailViewController.swift
@@ -13,15 +13,16 @@ class DetailViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var detailScrollView: UIScrollView!
     @IBOutlet weak var fullImageView: UIImageView!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    
     var selectedImageUrl = NSURL()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
         detailScrollView.delegate = self
         detailScrollView.maximumZoomScale = 5.0
         detailScrollView.minimumZoomScale = 1.0
-        // Do any additional setup after loading the view.
-        
     }
     
     override func viewDidLayoutSubviews() {
@@ -36,14 +37,17 @@ class DetailViewController: UIViewController, UIScrollViewDelegate {
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return self.fullImageView
     }
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func shareButtonPressed(_ sender: UIBarButtonItem) {
+        
+        let activityViewController = UIActivityViewController(activityItems: [fullImageView.image! as UIImage], applicationActivities: nil)
+        activityViewController.excludedActivityTypes = [.copyToPasteboard]
+//            activityItems: ["Check out this beer I liked using Beer Tracker.", url],
+//            applicationActivities: nil)
+        if let popoverPresentationController = activityViewController.popoverPresentationController {
+            popoverPresentationController.barButtonItem = (sender as! UIBarButtonItem)
+        }
+        present(activityViewController, animated: true, completion: nil)
+    
     }
-    */
-
 }
